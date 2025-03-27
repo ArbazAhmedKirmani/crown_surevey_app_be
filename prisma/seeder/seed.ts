cd68b211bdab053480dd25785762eeedee2852e8
Shiran Abbasi <shiran.abbasi@gmail.com>
2025-03-27 05:43:02 +0500

    changes again

--- prisma/seeder/seed.ts
+++ prisma/seeder/seed.ts
@@ -4,16 +4,20 @@ const prisma = new PrismaClient();
 
 async function main() {
   prisma.$connect();
-  await prisma.users.createMany({
-    data: [
-      {
-        name: "Admin",
-        email: "admin@gmail.com",
-        password:
-          "$2b$10$BTtyEfgcdnwSrnO0YxkBeOfsY3AnXjUkzhhEzqKLZRwsu.Cwo6WQa",
-      },
-    ],
+  const user = await prisma.users.findUnique({
+    where: { email: "admin@gmail.com" },
   });
+  if (!user)
+    await prisma.users.createMany({
+      data: [
+        {
+          name: "Admin",
+          email: "admin@gmail.com",
+          password:
+            "$2b$10$BTtyEfgcdnwSrnO0YxkBeOfsY3AnXjUkzhhEzqKLZRwsu.Cwo6WQa",
+        },
+      ],
+    });
 
   console.log("Seeding complete!");
 }
