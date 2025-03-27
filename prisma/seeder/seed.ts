b5f63e8d449ad14bc5b2a8cd3c81d22fc72037dc
Shiran Abbasi <shiran.abbasi@gmail.com>
2025-03-27 05:29:47 +0500

    aa

--- prisma/seeder/seed.ts
+++ prisma/seeder/seed.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  prisma.$connect();
   await prisma.users.createMany({
     data: [
       {
